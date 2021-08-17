7f842c4897bb495c46af28dcd519e1fcf124cb29
LordZee <l0rclz33@gmail.com>
2021-08-17 19:54:18 -0400

    finished exercise

--- numbers.js
+++ numbers.js
@@ -3,84 +3,41 @@ const favFacts = document.querySelector('.fav-facts')
 const multiFacts = document.querySelector('.multi-facts')
 const facts = document.querySelector('.facts')
 
-function get(url){
-    const request = new XMLHttpRequest();
-    return new Promise((resolve, reject) =>{
-        request.onload = function(){
-            if(request.readyState !== 4) return;
-        
-            if(request.status >= 200 && request.status < 300){
-                resolve({
-                    data: JSON.parse(request.response),
-                    status: request.status,
-                    request: request
-                })
-            }else{
-                reject({
-                    msg:"Server Error",
-                    status:request.status,
-                    request: request
-                })
-            }
-        }
-        
-        request.onerror = function handleError(){
-            reject("NETWORK ERROR!")
-            request = null;
-        };
-        
-        request.open('GET', url)    
-        request.send()        
-    })
+async function get(url){
+   return (url)   
 }
 
-get(BASE_URL + '/13/trivia?json')
-    .then(res => {
-        let li = document.createElement('li')
-        li.append(res.data.text)
-        favFacts.append(li)
-    })
-    .catch(err => {
-        console.log("ERROR!", err)
-    })
+async function getFavFact(){
+    let res = await axios.get(BASE_URL + '/13/trivia?json')
+    let li = document.createElement('li')
+    li.append(res.data.text)
+    favFacts.append(li)
+}
 
-get(BASE_URL + '/1..10?json')
-    .then(res => {
-        for(let val of Object.values(res.data)){
-            let li = document.createElement('li')
-            li.append(val)
-            facts.append(li)
-        }
-    })
-    .catch(err => {
-        console.log('ERROR!', err)
-    })
-    
-get(BASE_URL + '/13/trivia?json')
-    .then(res => {
+async function getFacts(){
+    let res = await axios.get(BASE_URL + '/1..10?json')
+    for(let val of Object.values(res.data)){
+        let li = document.createElement('li')
+        li.append(val)
+        facts.append(li)
+    }
+}
+
+async function getMultiFacts(){
+    let facts = await Promise.all([
+        axios.get(BASE_URL + '/13/trivia?json'),
+        axios.get(BASE_URL + '/13/trivia?json'),
+        axios.get(BASE_URL + '/13/trivia?json'),
+        axios.get(BASE_URL + '/13/trivia?json')
+    ])
+   
+    for(let res of facts){
         let li = document.createElement('li')
         li.append(res.data.text)
         multiFacts.append(li)
-        return get(BASE_URL + '/13/trivia?json')
-    })
-    .then(res => {
-        let li = document.createElement('li')
-        li.append(res.data.text)
-        multiFacts.append(li)
-        return get(BASE_URL + '/13/trivia?json')
-    })
-    .then(res => {
-        let li = document.createElement('li')
-        li.append(res.data.text)
-        multiFacts.append(li)
-        return get(BASE_URL + '/13/trivia?json')
-    })
-    .then(res => {
-        let li = document.createElement('li')
-        li.append(res.data.text)
-        multiFacts.append(li)
-        return get(BASE_URL + '/13/trivia?json')
-    })
-    .catch(err => {
-        console.log("ERROR!", err)
-    })
+    }
+}
+
+getFavFact()
+getFacts()
+getMultiFacts()
